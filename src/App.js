5f21894846ca5bb114c1b8b2b8d5f26b496f181e
Gem <mojeedibrahim13@gmail.com>
2025-10-03 10:02:04 -0700

    component, props, hooks done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 
 import './App.css';
-import imgprofile from './assets/images/gem.jpg'
+import { useState, useEffect } from 'react';
 
-const Person = (userName) => {
+// import imgprofile from './assets/images/gem.jpg'
 
-  return(
-    <>
-      <img src={imgprofile} alt='imgprofile' width="150px" />
-      <h1>Name: {userName.name}</h1>
-      <h2>Last Name: {userName.lastName}</h2>
-      <h3>Age: {userName.age}</h3>
-      <hr/>
-    </>
-  )
+// const Person = (userName) => component created to test the props is working well.{
 
-}
+//   return(
+//     <>
+//       <img src={imgprofile} alt='imgprofile' width="150px" />
+//       <h1>Name: {userName.name}</h1>
+//       <h2>Last Name: {userName.lastName}</h2>
+//       <h3>Age: {userName.age}</h3>
+//       <hr/>
+//     </>
+//   )
+
+// }
 
 function App() {
+
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    setCounter(10);
+  }, [])
+
   return (
     <div className="App">
-      <Person name={'Gem'} lastName={'Creation'} age={23} />
-      <Person name={'Ibrahim'} lastName={'Kola'} age={25} />
+      {/* <Person name={'Gem'} lastName={'Creation'} age={23} use for props/>
+      <Person name={'Ibrahim'} lastName={'Kola'} age={25} use for props/> */}
+
+      <button onClick={() => setCounter((prevCount) => prevCount - 1)}>-</button>
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter((prevCount) => prevCount + 1)}>+</button>
+
     </div>
   );
 }
